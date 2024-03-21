fccb87d631a705209f06ad72b640651dea503735
Viraj Ajay Joshi <virajajayjoshi@Virajs-MacBook-Pro.local>
2024-03-21 19:21:31 +0530

    fix: first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config"; // Optional if you want to use default theme
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <GluestackUIProvider config={config}>
+      <Component {...pageProps} />
+    </GluestackUIProvider>
+  );
 }
